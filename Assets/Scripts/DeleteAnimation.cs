237171346eb63cad27ba98f4d8888753cf0ff382
Mach1n1 <starshinin98@mail.ru>
2022-07-02 12:47:30 +0900

    Update DeleteAnimation.cs

--- Assets/Scripts/DeleteAnimation.cs
+++ Assets/Scripts/DeleteAnimation.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 public class DeleteAnimation : MonoBehaviour
 {    
+    //Я не смог интегрировать это в код
+    //Этот скрипт на префабе анимации
     void Start() => Destroy(this.gameObject, 1.5f);
 }
